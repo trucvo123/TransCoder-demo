8966e3337176a3a0f7979ba2f59281be333f76ab
TrucVo <84527503+trucvo123@users.noreply.github.com>
2023-01-11 18:03:41 +0700

    Update code_tokenizer.py

--- preprocessing/src/code_tokenizer.py
+++ preprocessing/src/code_tokenizer.py
@@ -21,7 +21,7 @@ from preprocessing.src.timeout import timeout, TimeoutError
 from sacrebleu import tokenize_v14_international
 
 TOK_NO_SPACE_BEFORE = {',', ';'}
-clang.cindex.Config.set_library_path('/path/to/lib/clang/libclang-14.so')
+clang.cindex.Config.set_library_path('/usr/lib/llvm-6.0/lib')
 STRINGS_AND_COMMENTS_TOKEN_KINDS = {TokenKind.LITERAL, TokenKind.COMMENT}
 logging.basicConfig(
     filename='timeout_cpp_tokenizer_examples.log', level=logging.DEBUG)
